8c17bf2150b2e5d026e59257831a5b35698eee0e
kkimmel1 <109098641+kkimmel1@users.noreply.github.com>
2023-04-25 17:18:06 -0700

    updated upload image button

--- static/js/homepage_scripts.js
+++ static/js/homepage_scripts.js
@@ -2,6 +2,7 @@ function previewImage() {
     const photoInput = document.getElementById("photo");
     const imagePlaceholder = document.getElementById("image-placeholder");
     const buttons = document.querySelectorAll('button.hide');
+    var label = document.querySelector('label[for="photo"]');
 
     if (photoInput.files && photoInput.files[0]) {
         const fileReader = new FileReader();
@@ -13,6 +14,7 @@ function previewImage() {
               style="max-width: 500px; max-height: 500px;
               display: inline-block; vertical-align: middle;">`;
             buttons.forEach(button => button.classList.remove('hide'));
+            label.textContent = 'Upload Different Image';
         };
 
         fileReader.readAsDataURL(photoInput.files[0]);
