3bb1dfc777d68739ef6958cde9f8485bfeff2a94
Javier Pacheco <jgpacheco2@usfca.edu>
2023-04-29 05:48:17 -0700

    CHange file name

--- application.py
+++ application.py
@@ -8,7 +8,7 @@ from werkzeug.utils import secure_filename
 from shutil import copyfile
 from enhancer import Picture
 
-app = Flask(__name__)
+application = Flask(__name__)
 
 def make_directory(directory):
     if not os.path.exists(directory):
@@ -18,18 +18,18 @@ make_directory("uploads/")
 make_directory("exported/")
 make_directory("output/")
 
-app.config["UPLOADED_PHOTOS_DEST"] = "uploads/"
-app.config["EXPORT_FOLDER"] = "exported/"
-app.config["OUTPUT_FOLDER"] = "output/"
+application.config["UPLOADED_PHOTOS_DEST"] = "uploads/"
+application.config["EXPORT_FOLDER"] = "exported/"
+application.config["OUTPUT_FOLDER"] = "output/"
 
 # Configure file uploads
 photos = UploadSet("photos", ALL)
-configure_uploads(app, photos)
+configure_uploads(application, photos)
 
 # Clear output directory
-files = os.listdir(app.config["OUTPUT_FOLDER"])
+files = os.listdir(application.config["OUTPUT_FOLDER"])
 for file in files:
-    file_path = os.path.join(app.config["OUTPUT_FOLDER"], file)
+    file_path = os.path.join(application.config["OUTPUT_FOLDER"], file)
     os.unlink(file_path)
 
 picture_instance = None
@@ -39,15 +39,15 @@ if RUN_MODEL:
     from models.lama.lama import get_model_instance, make_prediction
     model, predict_config = get_model_instance()
 
-@app.route("/static/<path:filename>")
+@application.route("/static/<path:filename>")
 def serve_static(filename):
     return send_from_directory("static", filename)
 
-@app.route("/")
+@application.route("/")
 def index():
     return render_template("index.html")
 
-@app.route("/upload", methods=["POST"])
+@application.route("/upload", methods=["POST"])
 def upload():
     if "photo" not in request.files:
         return redirect(request.url)
@@ -61,8 +61,8 @@ def upload():
 
         # Save the uploaded image as original_mask_0.png in the "output" directory
         original_mask_file = secure_filename("original_image_mask0.png")
-        original_mask_path = os.path.join(app.config["OUTPUT_FOLDER"], original_mask_file)
-        uploaded_image_path = os.path.join(app.config["UPLOADED_PHOTOS_DEST"], filename)
+        original_mask_path = os.path.join(application.config["OUTPUT_FOLDER"], original_mask_file)
+        uploaded_image_path = os.path.join(application.config["UPLOADED_PHOTOS_DEST"], filename)
         copyfile(uploaded_image_path, original_mask_path)
 
         # Check the value of the "action" parameter
@@ -76,19 +76,19 @@ def upload():
 
     return redirect(url_for("index"))
 
-@app.route("/uploads/<filename>")
+@application.route("/uploads/<filename>")
 def uploaded_file(filename):
     return render_template("eraser.html", filename=filename)
 
-@app.route("/image/<filename>")
+@application.route("/image/<filename>")
 def serve_image(filename):
-    return send_from_directory(app.config["UPLOADED_PHOTOS_DEST"], filename)
+    return send_from_directory(application.config["UPLOADED_PHOTOS_DEST"], filename)
 
-@app.route("/output/<filename>")
+@application.route("/output/<filename>")
 def serve_output(filename):
     return send_from_directory("output", filename)
 
-@app.route("/export", methods=["POST"])
+@application.route("/export", methods=["POST"])
 def export():
     img_data = request.form["imgData"]
     img_type = request.form["imgType"]
@@ -97,17 +97,17 @@ def export():
     # Decode and save the image
     img_data = img_data.split(",")[1]
     img_data = base64.b64decode(img_data)
-    img_path = os.path.join(app.config["EXPORT_FOLDER"], secure_filename(img_name))
+    img_path = os.path.join(application.config["EXPORT_FOLDER"], secure_filename(img_name))
 
     with open(img_path, "wb") as f:
         f.write(img_data)
 
     return "Image saved", 200
 
-@app.route("/process", methods=["POST"])
+@application.route("/process", methods=["POST"])
 def process():
-    input_image_dir = app.config["EXPORT_FOLDER"] 
-    output_dir = app.config["OUTPUT_FOLDER"] 
+    input_image_dir = application.config["EXPORT_FOLDER"]
+    output_dir = application.config["OUTPUT_FOLDER"]
     os.makedirs(output_dir, exist_ok=True)
     stack_position = request.form["stackPos"]
 
@@ -132,15 +132,15 @@ def process():
     return {"outputImageB64": output_image_b64}
 
 
-@app.route("/enhancer/<filename>")
+@application.route("/enhancer/<filename>")
 def enhancer(filename):
     global picture_instance
-    uploaded_image_path = os.path.join(app.config["UPLOADED_PHOTOS_DEST"], filename)
+    uploaded_image_path = os.path.join(application.config["UPLOADED_PHOTOS_DEST"], filename)
     picture_instance = Picture(uploaded_image_path)
     return render_template("enhancer.html", filename=filename)
 
 
-@app.route("/enhance/<function_name>", methods=["POST"])
+@application.route("/enhance/<function_name>", methods=["POST"])
 def enhance(function_name):
     global picture_instance
     if not picture_instance:
@@ -158,4 +158,4 @@ def enhance(function_name):
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    application.run(debug=True)
