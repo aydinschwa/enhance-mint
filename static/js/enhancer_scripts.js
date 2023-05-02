85160045d6809ec445faf1e04635e930eaf19ab8
aydinschwa <aydinschwa@gmail.com>
2023-05-01 19:50:25 -0700

    add filters and fix resizing for enhancer

--- static/js/enhancer_scripts.js
+++ static/js/enhancer_scripts.js
@@ -8,9 +8,17 @@ const saturationSlider = document.getElementById("saturation");
 const exposureSlider = document.getElementById("exposure");
 const sharpenSlider = document.getElementById("sharpen");
 const vibranceSlider = document.getElementById("vibrance");
-const noiseSlider = document.getElementById("noise");
 const sepiaSlider = document.getElementById("sepia");
-const resetButton = document.getElementById("resetFilters")
+const resetButton = document.getElementById("resetFilters");
+const vintageButton = document.getElementById("vintage-btn");
+const lomoButton = document.getElementById("lomo-btn");
+const clarityButton = document.getElementById("clarity-btn");
+const sinCityButton = document.getElementById("sincity-btn");
+const crossProcessButton = document.getElementById("crossprocess-btn");
+const pinholeButton = document.getElementById("pinhole-btn");
+const nostalgiaButton = document.getElementById("nostalgia-btn");
+const herMajestyButton = document.getElementById("majestic-btn");
+
 
 // resize image if it's larger than maxWidth, maxHeight 
 const imgRatio = imgElement.width / imgElement.height;
@@ -27,8 +35,12 @@ newHeight = maxImageHeight;
 newWidth = newHeight * imgRatio;
 }
 
-imgElement.width = newWidth;
-imgElement.height = newHeight;
+Caman(imgElement, function () {
+  this.resize({
+    width: newWidth,
+    height: newHeight
+  }).render();
+});
 
 saveImageButton.addEventListener('click', function() {
     saveImageButton.href = imgElement.src;
@@ -45,8 +57,7 @@ function applyEffects() {
       this.saturation(saturationSlider.value);
       this.exposure(exposureSlider.value);
       this.sharpen(sharpenSlider.value);
-      this.noise(noiseSlider.value);
-      this.sepia(sepia.value);
+      this.sepia(sepiaSlider.value);
       this.render();
     });
   }
@@ -57,7 +68,6 @@ saturationSlider.addEventListener("change", applyEffects);
 exposureSlider.addEventListener("change", applyEffects);
 sharpenSlider.addEventListener("change", applyEffects);
 vibranceSlider.addEventListener("change", applyEffects);
-noiseSlider.addEventListener("change", applyEffects);
 sepiaSlider.addEventListener("change", applyEffects);
 
 
@@ -67,7 +77,6 @@ resetButton.addEventListener("click", () => {
     exposureSlider.value = 0;
     sharpenSlider.value = 0;
     vibranceSlider.value = 0;
-    noiseSlider.value = 0;
     sepiaSlider.value = 0;
     const imgElement = document.querySelector("#image-container img") || document.querySelector("#image-container canvas");
     Caman(imgElement, function () {
@@ -77,8 +86,71 @@ resetButton.addEventListener("click", () => {
       this.saturation(saturationSlider.value);
       this.exposure(exposureSlider.value);
       this.sharpen(sharpenSlider.value);
-      this.noise(noiseSlider.value);
       this.sepia(sepia.value);
       this.render();
     });
 })
+
+vintageButton.addEventListener("click", function() {
+  const imgElement = document.querySelector("#image-container img") || document.querySelector("#image-container canvas");
+  Caman(imgElement, function() {
+    this.revert(false);
+    this.vintage().render();
+  });
+});
+
+lomoButton.addEventListener("click", function() {
+  const imgElement = document.querySelector("#image-container img") || document.querySelector("#image-container canvas");
+  Caman(imgElement, function() {
+    this.revert(false);
+    this.lomo().render();
+  });
+});
+
+clarityButton.addEventListener("click", function() {
+  const imgElement = document.querySelector("#image-container img") || document.querySelector("#image-container canvas");
+  Caman(imgElement, function() {
+    this.revert(false);
+    this.clarity().render();
+  });
+});
+
+sinCityButton.addEventListener("click", function() {
+  const imgElement = document.querySelector("#image-container img") || document.querySelector("#image-container canvas");
+  Caman(imgElement, function() {
+    this.revert(false);
+    this.sinCity().render();
+  });
+});
+
+crossProcessButton.addEventListener("click", function() {
+  const imgElement = document.querySelector("#image-container img") || document.querySelector("#image-container canvas");
+  Caman(imgElement, function() {
+    this.revert(false);
+    this.crossProcess().render();
+  });
+});
+
+pinholeButton.addEventListener("click", function() {
+  const imgElement = document.querySelector("#image-container img") || document.querySelector("#image-container canvas");
+  Caman(imgElement, function() {
+    this.revert(false);
+    this.sinCity().render();
+  });
+});
+
+nostalgiaButton.addEventListener("click", function() {
+  const imgElement = document.querySelector("#image-container img") || document.querySelector("#image-container canvas");
+  Caman(imgElement, function() {
+    this.revert(false);
+    this.nostalgia().render();
+  });
+});
+
+herMajestyButton.addEventListener("click", function() {
+  const imgElement = document.querySelector("#image-container img") || document.querySelector("#image-container canvas");
+  Caman(imgElement, function() {
+    this.revert(false);
+    this.herMajesty().render();
+  });
+});
